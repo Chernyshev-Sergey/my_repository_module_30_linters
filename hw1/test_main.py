2bd8473263d18ce44ca08eacd944b1a4121b7054
Sergey Chernyshev <schern2016@.gmail.com>
2025-09-23 09:12:31 +0500

    py_adv_module_30_ci_linters_hw1

---
+++ hw1/test_main.py
@@ -0,0 +1,47 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main_recipes():
+    response = client.get("/recipes")
+    assert response.status_code == 200
+    print(response.json())
+    assert response.json() == [
+        {
+            "id": 3,
+            "name_of_the_dish": "каша рисовая",
+            "cooking_time_in_minutes": "61",
+            "number_of_views": 12,
+        },
+        {
+            "id": 1,
+            "name_of_the_dish": "Борщ",
+            "cooking_time_in_minutes": "60",
+            "number_of_views": 0,
+        },
+        {
+            "id": 2,
+            "name_of_the_dish": "Рассольник",
+            "cooking_time_in_minutes": "50",
+            "number_of_views": 0,
+        },
+    ]
+
+
+def test_read_main_recipes_id():
+    response = client.get("/recipes/3")
+    assert response.status_code == 200
+    print(response.json())
+    assert response.json() == {
+        "name_of_the_dish": "каша рисовая",
+        "cooking_time_in_minutes": "61",
+        "list_of_ingredients": "рис",
+        "description": "нет описания",
+    }
+
+
+if __name__ == "__main__":
+    test_read_main_recipes()
+    test_read_main_recipes_id()
